76cf45cba292b04b1cf6115946ab74c7aedda73c
Stefano <s.cislaghi@gmail.com>
2024-04-27 14:15:12 +0100

    Updates (#12)
    * chore: dependencies
    
    * fix: metadata

--- next.config.js
+++ next.config.js
@@ -3,14 +3,17 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "dummyimage.com",
-      "is1-ssl.mzstatic.com",
-      "is2-ssl.mzstatic.com",
-      "is3-ssl.mzstatic.com",
-      "is4-ssl.mzstatic.com",
-      "is5-ssl.mzstatic.com",
-      "is-ssl.mzstatic.com.itunes-apple.com.akadns.net",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.mzstatic.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.itunes-apple.com.akadns.net',
+        port: '',
+      },
     ],
   },
 };
