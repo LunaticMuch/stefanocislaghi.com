0dbdfb1d56dc7c7878f72ef0f8ddd2f96f422791
Stefano <s.cislaghi@gmail.com>
2025-02-01 20:44:54 +0000

    Introduce new component for Github contributions (#17)
    * feat: contributions
    
    * feat: metadata
    
    * fix: linting explicit any

---
+++ components/Contributions.tsx
@@ -0,0 +1,56 @@
+"use client";
+import useSWR from "swr";
+import Link from "next/link";
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Contributions() {
+  const { data } = useSWR("api/contributions", fetcher);
+  return (
+    <>
+      <div className="">
+        <p className="font-bold uppercase">Contributions</p>
+        <ul className="grid gap-1">
+          {data?.map((contribution, key) => (
+            <li className="flex justify-between" key={key}>
+              <Link
+                className="overflow-hidden truncate text-ellipsis max-w-48 group"
+                href={contribution.url}
+                target="_blank"
+              >
+                {contribution.repository}
+                {contribution.type === 'pr' ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    className="size-4 inline-block align-[-2px] ml-1 group-hover:animate-bounce"
+                  >
+                    <path d="M16 19.25a3.25 3.25 0 1 1 6.5 0 3.25 3.25 0 0 1-6.5 0Zm-14.5 0a3.25 3.25 0 1 1 6.5 0 3.25 3.25 0 0 1-6.5 0Zm0-14.5a3.25 3.25 0 1 1 6.5 0 3.25 3.25 0 0 1-6.5 0ZM4.75 3a1.75 1.75 0 1 0 .001 3.501A1.75 1.75 0 0 0 4.75 3Zm0 14.5a1.75 1.75 0 1 0 .001 3.501A1.75 1.75 0 0 0 4.75 17.5Zm14.5 0a1.75 1.75 0 1 0 .001 3.501 1.75 1.75 0 0 0-.001-3.501Z" />
+                    <path d="M13.405 1.72a.75.75 0 0 1 0 1.06L12.185 4h4.065A3.75 3.75 0 0 1 20 7.75v8.75a.75.75 0 0 1-1.5 0V7.75a2.25 2.25 0 0 0-2.25-2.25h-4.064l1.22 1.22a.75.75 0 0 1-1.061 1.06l-2.5-2.5a.75.75 0 0 1 0-1.06l2.5-2.5a.75.75 0 0 1 1.06 0ZM4.75 7.25A.75.75 0 0 1 5.5 8v8A.75.75 0 0 1 4 16V8a.75.75 0 0 1 .75-.75Z" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-4 inline-block align-[-2px] ml-1 group-hover:animate-bounce"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                    />
+                  </svg>
+                )}
+              </Link>
+              <div>{contribution.date}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
