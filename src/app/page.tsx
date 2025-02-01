27a95b3e6c71059209e80ba54f32d5d280ce2960
Stefano Cislaghi <s.cislaghi@gmail.com>
2025-02-01 19:58:51 +0000

    feat: metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,44 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title:
-    "Stefano Cislaghi - Enterprise Architect, Visionary Leader, Engineering Mentor",
+    "Stefano Cislaghi | Engineering Visionary, Enterprise Architect & Open Innovation Advocate",
+  description:
+    "Innovative Technology Architect with over 10 years of experience driving digital transformation in global financial institutions. Expert in modernizing enterprise systems, building high-performing engineering cultures, and bridging technical expertise with business objectives. A trusted technology partner specializing in compliance, risk management, and value co-creation to deliver future-proof solutions.",
   robots: "follow, index",
   category: "technology",
   metadataBase: new URL("https://stefanocislaghi.com"),
+  openGraph: {
+    url: "https://stefanocislaghi.com",
+    title:
+      "Stefano Cislaghi | Engineering Visionary, Enterprise Architect & Open Innovation Advocate",
+    description:
+      "Innovative Technology Architect with over 10 years of experience driving digital transformation in global financial institutions. Expert in modernizing enterprise systems, building high-performing engineering cultures, and bridging technical expertise with business objectives. A trusted technology partner specializing in compliance, risk management, and value co-creation to deliver future-proof solutions.",
+    images: [
+      {
+        url: "https://stefanocislaghi.com/stefano_og.jpg",
+        width: 512,
+        height: 512,
+        alt: "Stefano Cislaghi",
+        type: "image/jpeg",
+      },
+    ],
+    siteName:
+      "Stefano Cislaghi | Engineering Visionary, Enterprise Architect & Open Innovation Advocate",
+  },
 };
 
 export default function Home() {
   return (
     <>
-        <div className="inset-0 -z-10 size-full ">
-          <div className="container mx-auto my-5 grid max-w-[360px] gap-14">
-            <Headline />
-            <Experience />
-            <Contacts />
-            <Contributions />
-            <Footer />
-          </div>
+      <div className="inset-0 -z-10 size-full ">
+        <div className="container mx-auto my-5 grid max-w-[360px] gap-14">
+          <Headline />
+          <Experience />
+          <Contacts />
+          <Contributions />
+          <Footer />
         </div>
+      </div>
     </>
   );
 }
